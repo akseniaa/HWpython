14fd85257eb8ed963255eb9bee735e56b5cccfba
akseniaa <65546005+akseniaa@users.noreply.github.com>
2020-12-22 21:09:28 +0500

    Update ExcelTaskV5.py

--- ExcelTaskV5.py
+++ ExcelTaskV5.py
@@ -3,7 +3,5 @@ import os
 
 reader = pd.read_excel(os.path.join("excel/sheets.xlsx"), engine='openpyxl', sheet_name=None)
 for key in reader.keys():
-sheet = pd.read_excel("excel/sheets.xlsx", engine='openpyxl', sheet_name=key)
-sheet.to_csv(f"{key}.csv")
-
-
+  sheet = pd.read_excel("excel/sheets.xlsx", engine='openpyxl', sheet_name=key)
+  sheet.to_csv(f"{key}.csv")
